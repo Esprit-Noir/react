ed9edee8326ac58258b52e962d7dd9880bc82559
Esprit-Noir <mamadouthiandoum96@gmail.com>
2023-10-10 09:54:18 +0000

    refacto

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-const Article = ({article}) => {
-    return (
-            <div className={`post-box ${article.primary_category.slug}`} >
-                <img src={article.jetpack_featured_media_url} alt="" className="post-img" />
-                <h2 className="category">{article.primary_category.name}</h2>
-                <a href={`/articles/${article.id}`} className="post-title">{article.title.rendered}</a>
-                <span className="post-date">{article.date}</span>
-                <p className="post-description" dangerouslySetInnerHTML={{ __html: article.excerpt.rendered }}></p>
-                <div className="profile">
-                    <img src="images/testi1.jpg" alt="" className="profile-img" />
-                    <span className="profile-name">MKHB</span>
-                </div>
-            </div>
-    );
-}
+const Article = ({ article }) => {
+  return (
+    <div className={`post-box ${article.primary_category.slug}`}>
+      <img
+        src={article.jetpack_featured_media_url}
+        alt=""
+        className="post-img"
+      />
+      <h2 className="category">{article.primary_category.name}</h2>
+      <a href={`/articles/${article.id}`} className="post-title">
+        {article.title.rendered}
+      </a>
+      <span className="post-date">{article.date}</span>
+      <p
+        className="post-description"
+        dangerouslySetInnerHTML={{ __html: article.excerpt.rendered }}
+      ></p>
+      <div className="profile">
+        <img src="images/testi1.jpg" alt="" className="profile-img" />
+        <span className="profile-name">MKHBhdd</span>
+      </div>
+    </div>
+  );
+};
 
 export default Article;
