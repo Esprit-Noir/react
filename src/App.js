c0b7b1d04bfadbfaa722ff656181a01c9f99d837
mariedelza <127205165+mariedelza@users.noreply.github.com>
2023-10-10 11:49:54 +0200

    Revert "refactoring codes, adding react-router-dom and adding make acces to a…"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from "./pages/Home";
-import ArticleDetail from "./pages/ArticleDetail";
+import Home from './pages/Home';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://techcrunch.com/wp-json/wp/v2/posts')
+      .then(response => response.json())
+      .then(articles => this.setState({ articles }))
+      .catch(error => console.error(error));
+  }
+
   render() {
     return (
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/articles/:id" element={<ArticleDetail />} />
-          </Routes>
-        </BrowserRouter>
+      <div className="App">
+        <Home articles={this.state.articles} />
+      </div>
     );
   }
 }
